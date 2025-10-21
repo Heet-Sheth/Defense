10964aaebbaf9af18da8cf8ff2f86d36828cbd59
Heet Sheth <97078945+Heet-Sheth@users.noreply.github.com>
2025-10-21 22:44:06 +0530

    Background color changer with a color picker.

---
+++ JS_PROJECT/project2/index.js
@@ -0,0 +1,15 @@
+const changerButton = [...document.getElementsByClassName("button")];
+const picker = document.getElementById("color-picker");
+const body = [...document.getElementsByTagName("body")][0];
+
+changerButton.forEach((button) =>
+  button.addEventListener(
+    "click",
+    () => (body.style.backgroundColor = button.classList[1])
+  )
+);
+
+picker.addEventListener(
+  "input",
+  () => (body.style.backgroundColor = picker.value)
+);
