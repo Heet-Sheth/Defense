d1b7b527bf530375792d3668547700ebb5054dcf
Heet Sheth <97078945+Heet-Sheth@users.noreply.github.com>
2025-10-22 14:42:50 +0530

    Mini Project: Income tax Calculator

---
+++ JS_PROJECT/project4/index.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form");
+const income = document.querySelector("input");
+const tax = document.querySelector("p");
+
+const slabs = [
+  { slab: 2400000, taxRate: 0.3 },
+  { slab: 200000, taxRate: 0.25 },
+  { slab: 1600000, taxRate: 0.2 },
+  { slab: 1200000, taxRate: 0.15 },
+];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let amount = parseInt(income.value);
+  let sum = 0;
+
+  slabs.forEach(({ slab, taxRate }) => {
+    if (amount > slab) {
+      sum += (amount - slab) * taxRate;
+      amount = slab;
+    }
+  });
+
+  tax.textContent = `Your total tax is ${sum}₹`;
+});
